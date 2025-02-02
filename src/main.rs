77f292ced70dde7b83b8e4f5db0d61e6bc791885
Seth Price <sethrprice@me.com>
2025-02-02 21:50:59 +0000

    created Lexer struct with get_tok() method

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use inkwell::context::Context;
+// use inkwell::context::Context;
 
 fn main() {
-    let context = Context::create();
-    let module = context.create_module("kaleidoscope");
-    let builder = context.create_builder();
+    // let context = Context::create();
+    // let module = context.create_module("kaleidoscope");
+    // let builder = context.create_builder();
 
-    println!("LLVM Module Created: {:?}", module);
+    // println!("LLVM Module Created: {:?}", module);
+
+    let source = "def foo 3.14 extern";
+    let mut lexer = Lexer::new(source);
+
+    loop {
+        let token = lexer.get_tok();
+        println!("{:?}", token);
+        if token == Token::Eof {
+            break;
+        }
+    }
 }
 
+#[derive(Debug, PartialEq)]
 enum Token {
     // End of file
     Eof,
@@ -50,7 +62,43 @@ impl<'a> Lexer<'a> {
             self.advance()
         }
 
-        // recognise identifiers
-        if self.current_char.map_or(false, |c| c.is_alphabetic()) {}
+        // recognise identifiers and keywords
+        if let Some(c1) = self.current_char {
+            if c1.is_alphabetic() {
+                let mut identifier = String::new();
+                while let Some(c2) = self.current_char {
+                    if c2.is_alphabetic() {
+                        identifier.push(c2);
+                        self.advance();
+                    } else {
+                        break;
+                    }
+                }
+                return match identifier.as_str() {
+                    "def" => Token::Def,
+                    "extern" => Token::Extern,
+                    _ => Token::Identifier(identifier),
+                };
+            }
+
+            // recognise numbers
+            if c1.is_digit(10) || c1 == '.' {
+                let mut num_str = String::new();
+                while let Some(c2) = self.current_char {
+                    if c2.is_digit(10) || c2 == '.' {
+                        num_str.push(c2);
+                        self.advance();
+                    } else {
+                        break;
+                    }
+                }
+                return Token::Number(num_str.parse().unwrap());
+            }
+
+            // single character tokens e.g. operators, punctuation
+            self.advance();
+            return Token::Char(c1);
+        }
+        return Token::Eof;
     }
 }
